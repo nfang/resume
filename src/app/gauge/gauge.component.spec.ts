63b07cc33343777e58b09ffdd461f7f57a29668d
Nathan Fang <nyyrikkifang@gmail.com>
2016-10-03 12:15:40 +1100

    Bug fixes and improvements
    fix: add favicons
    test: add unit tests for GaugeComponent

--- src/app/gauge/gauge.component.spec.ts
+++ src/app/gauge/gauge.component.spec.ts
@@ -7,5 +7,18 @@ describe('Component: Gauge', () => {
   it('should create an instance', () => {
     let component = new GaugeComponent();
     expect(component).toBeTruthy();
+    expect(component.min).toBe(0);
+  });
+
+  it('should calculate the percentage to fill', () => {
+    let component = new GaugeComponent();
+    expect(component.percentage).toBe('0.0%');
+
+    component.max = 100;
+    component.value = 60;
+    expect(component.percentage).toBe('60.0%');
+
+    component.value = 40.45;
+    expect(component.percentage).toBe('40.5%');
   });
 });
